ceb520e70302ce09ce94c36596ef87b7ec2c1620
Dharma Guadeloupe <67554685+DharmaKarmaStar@users.noreply.github.com>
2021-07-08 14:56:26 +0100

    exercises E-array-map using arrow functions

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -2,3 +2,11 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+
+//PSUEDO CODE
+//*map goes over every item in array
+//*multiply each item by 100
+
+const multipliedNumbers = numbers.map(number => number * 100);
+
+console.log(multipliedNumbers);
